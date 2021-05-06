0d4cb1f4ba4e3f401faa7d1be0e711b2d7c10d1a
Ismail <codeismail@gmail.com>
2021-05-06 17:12:57 +0100

    update completion screen

--- app/src/main/java/ng/max/vams/ui/shared/CompleteRegistrationFragment.kt
+++ app/src/main/java/ng/max/vams/ui/shared/CompleteRegistrationFragment.kt
@@ -5,20 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import ng.max.vams.R
+import ng.max.vams.databinding.FragmentCompleteRegistrationBinding
 
 class CompleteRegistrationFragment : Fragment() {
 
-    //private lateinit var bnd:suspend Com
+    private lateinit var bnd: FragmentCompleteRegistrationBinding
+    private val args: CompleteRegistrationFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complete_registration, container, false)
+    ): View {
+        bnd = FragmentCompleteRegistrationBinding.inflate(inflater, container, false)
+        return bnd.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bnd.successMessageTv.text = getString(R.string.you_have_successfully_label, args.movementType)
+        bnd.vehicleIdTv.text = args.vehicleId
+
+        bnd.backToDashboardBtn.setOnClickListener {
+            findNavController().navigate(R.id.homeFragment)
+        }
     }
 }
