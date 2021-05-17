f3c928b62e7d7e768a3c250bcc516c5e0151d8ed
Ismail <codeismail@gmail.com>
2021-05-17 09:42:02 +0100

    Add Location and VehicleType Entities and Dao to AppDatabase

--- app/src/main/java/ng/max/vams/data/local/AppDatabase.kt
+++ app/src/main/java/ng/max/vams/data/local/AppDatabase.kt
@@ -4,13 +4,22 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import ng.max.vams.data.local.dao.LocationDao
+import ng.max.vams.data.local.dao.ReasonDao
+import ng.max.vams.data.local.dao.VehicleDao
+import ng.max.vams.data.local.dao.VehicleTypeDao
+import ng.max.vams.data.remote.response.Location
 import ng.max.vams.data.remote.response.Reason
+import ng.max.vams.data.remote.response.VehicleType
 
-@Database(version = 1, entities = [DbVehicle::class, Reason::class], exportSchema = false)
+@Database(version = 1, entities = [DbVehicle::class, Reason::class,
+    Location::class, VehicleType::class], exportSchema = false)
 abstract class AppDatabase : RoomDatabase(){
 
     abstract fun vehicleDao(): VehicleDao
     abstract fun reasonDao(): ReasonDao
+    abstract fun locationDao(): LocationDao
+    abstract fun vehicleTypeDao(): VehicleTypeDao
 
     companion object {
         private const val DB_NAME = "max-vams.db"
