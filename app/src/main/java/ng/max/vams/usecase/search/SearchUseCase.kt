1f1fc3c55be678446432e819561417a8b95338f9
Ismail <codeismail@gmail.com>
2021-05-20 14:15:58 +0100

    Update error handling

--- app/src/main/java/ng/max/vams/usecase/search/SearchUseCase.kt
+++ app/src/main/java/ng/max/vams/usecase/search/SearchUseCase.kt
@@ -2,26 +2,18 @@ package ng.max.vams.usecase.search
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ng.max.vams.data.remote.RemoteDataSource
 import ng.max.vams.data.remote.response.Vehicle
-import ng.max.vams.data.remote.services.VehicleService
 import ng.max.vams.data.wrapper.Result
 import javax.inject.Inject
 
 class SearchUseCase @Inject constructor(
-    private val vehicleService: VehicleService
+    private val remoteDataSource: RemoteDataSource
 ) {
 
     suspend fun invoke(term: String): Flow<Result<List<Vehicle>>> {
         return flow {
-            try {
-                val response = vehicleService.search(term)
-                if (response.isSuccessful){
-                    val vehicles = response.body()?.getData()?.vehicles
-                    emit(Result.Success(vehicles!!))
-                }
-            }catch (ex: Exception){
-                emit(Result.Error(ex.localizedMessage!!))
-            }
+            emit(remoteDataSource.getSearchResult(term))
         }
     }
 }
