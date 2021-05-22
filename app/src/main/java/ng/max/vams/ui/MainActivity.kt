56f2ef74594ef4ce5ed921cf20abc3e2808eb771
Ismail <codeismail@gmail.com>
2021-05-22 11:31:43 +0100

    use view binding where possible instead of databinding

--- app/src/main/java/ng/max/vams/ui/MainActivity.kt
+++ app/src/main/java/ng/max/vams/ui/MainActivity.kt
@@ -1,26 +1,25 @@
 package ng.max.vams.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import ng.max.vams.R
 import ng.max.vams.databinding.ActivityMainBinding
-import ng.max.vams.util.gone
-import ng.max.vams.util.show
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var bnd: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_VAMS)
         super.onCreate(savedInstanceState)
-        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        bnd = ActivityMainBinding.inflate(layoutInflater)
+        val view = bnd.root
+        setContentView(view)
         handleNavController()
     }
 
@@ -28,9 +27,9 @@ class MainActivity : AppCompatActivity() {
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         val listener = NavController.OnDestinationChangedListener{ _, destination, _ ->
             if (destination.id == R.id.loginFragment || destination.id == R.id.appDialogFragment){
-//                activityMainBinding.appToolbar.gone()
+//                bnd.appToolbar.gone()
             }else{
-//                activityMainBinding.appToolbar.show()
+//                bnd.appToolbar.show()
             }
         }
         navController.addOnDestinationChangedListener(listener)
