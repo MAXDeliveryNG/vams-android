85915c43dfd3dcb13d289603951c889d6a4a77fd
Ismail <codeismail@gmail.com>
2021-05-24 11:57:21 +0100

    resolve merge conflict

--- app/src/main/java/ng/max/vams/ui/MainActivity.kt
+++ app/src/main/java/ng/max/vams/ui/MainActivity.kt
@@ -2,8 +2,6 @@ package ng.max.vams.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
