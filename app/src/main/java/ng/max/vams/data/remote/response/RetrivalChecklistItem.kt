f23ef338eebd57c1485ac890d8226cc4208bf841
Decagon <decagon@Love-dovey.local>
2021-11-21 14:25:40 +0100

    initial checklist added endpoint

---
+++ app/src/main/java/ng/max/vams/data/remote/response/RetrivalChecklistItem.kt
@@ -0,0 +1,23 @@
+package ng.max.vams.data.remote.response
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity
+data class RetrivalChecklistItem(
+
+    @PrimaryKey
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("slug")
+    val slug: String,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("updated_at")
+    val updatedAt: String
+)
+
+
