9f488b20abb4a4bac760b80b0184f99670b33463
Ismail <codeismail@gmail.com>
2021-11-28 01:12:28 +0100

    Update failing test

--- app/src/androidTest/java/ng/max/vams/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ng/max/vams/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package ng.max.vams
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ng.max.vams", appContext.packageName)
+        assertEquals("ng.max.vams.staging.debug", appContext.packageName)
     }
 }
